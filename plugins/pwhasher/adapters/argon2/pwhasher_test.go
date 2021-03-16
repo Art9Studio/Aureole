62a141f41d1a2afd6fa3867f16e0e6fde86f3a89
Igor Fominykh <igos.321@gmail.com>
2021-03-16 21:03:41 +0300

    F32: Refactor config: pure app configs, improved loading and validation: Fixed some tests

--- plugins/pwhasher/adapters/argon2/pwhasher_test.go
+++ plugins/pwhasher/adapters/argon2/pwhasher_test.go
@@ -5,95 +5,104 @@ import (
 	"testing"
 )
 
+// todo: remove when default config will exist in plugin
+var DefaultConfig = &Conf{
+	Kind:        "argon2i",
+	Iterations:  3,
+	Parallelism: 2,
+	SaltLen:     16,
+	KeyLen:      32,
+	Memory:      32 * 1024,
+}
+
 func TestArgon2_Hash(t *testing.T) {
-	type fields struct {
-		conf *Conf
-	}
 	type args struct {
 		data string
 	}
 	tests := []struct {
 		name    string
-		fields  fields
+		conf    *Conf
 		args    args
 		wantErr bool
 	}{
 		{
 			name:    "argon2i default",
-			fields:  fields{conf: DefaultConfig},
+			conf:    DefaultConfig,
 			args:    args{data: "qwerty"},
 			wantErr: false,
 		},
 		{
 			name: "argon2i 32/64",
-			fields: fields{conf: &Conf{
+			conf: &Conf{
 				Kind:        "argon2i",
 				Iterations:  4,
 				Parallelism: 4,
 				SaltLen:     32,
 				KeyLen:      64,
 				Memory:      32 * 1024,
-			}},
+			},
 			args:    args{data: "hdg36*/*12bd6"},
 			wantErr: false,
 		},
 		{
 			name: "argon2i 64/128",
-			fields: fields{conf: &Conf{
+			conf: &Conf{
 				Kind:        "argon2i",
 				Iterations:  3,
 				Parallelism: 2,
 				SaltLen:     64,
 				KeyLen:      128,
 				Memory:      32 * 1024,
-			}},
+			},
 			args:    args{data: "hdg36*/*12bd6"},
 			wantErr: false,
 		},
 		{
 			name: "argon2id default",
-			fields: fields{conf: &Conf{
+			conf: &Conf{
 				Kind:        "argon2id",
 				Iterations:  3,
 				Parallelism: 2,
 				SaltLen:     16,
 				KeyLen:      32,
 				Memory:      32 * 1024,
-			}},
+			},
 			args:    args{data: "qwerty"},
 			wantErr: false,
 		},
 		{
 			name: "argon2id 32/64",
-			fields: fields{conf: &Conf{
+			conf: &Conf{
 				Kind:        "argon2id",
 				Iterations:  1,
 				Parallelism: 2,
 				SaltLen:     32,
 				KeyLen:      64,
 				Memory:      32 * 1024,
-			}},
+			},
 			args:    args{data: "hyy167hsjsj-12g"},
 			wantErr: false,
 		},
 		{
 			name: "argon2id 64/128",
-			fields: fields{conf: &Conf{
+			conf: &Conf{
 				Kind:        "argon2id",
 				Iterations:  3,
 				Parallelism: 2,
 				SaltLen:     64,
 				KeyLen:      128,
 				Memory:      32 * 1024,
-			}},
+			},
 			args:    args{data: "hyy167hsjsj-12g"},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a := Argon2{
-				Ctx: tt.fields.conf,
+			a, err := initAdapter(nil, tt.conf)
+			if err != nil {
+				t.Errorf("initAdapter() error = %v", err)
+				return
 			}
 			got, err := a.HashPw(tt.args.data)
 			if (err != nil) != tt.wantErr {
@@ -116,14 +125,14 @@ func TestArgon2_Compare(t *testing.T) {
 	}
 	tests := []struct {
 		name    string
-		fields  fields
+		conf    *Conf
 		args    args
 		want    bool
 		wantErr bool
 	}{
 		{
-			name:   "argon2i valid data",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2i valid data",
+			conf: DefaultConfig,
 			args: args{
 				data: "qwerty",
 				hash: "$argon2i$v=19$m=32768,t=3,p=2$VDkrfTNOys4cBijO2rNTBw$2NP3RaDtHrXrMU+kKlcyTvxjyZOfHYoSAxmUjxS4w1Q",
@@ -132,8 +141,8 @@ func TestArgon2_Compare(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:   "argon2i invalid data",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2i invalid data",
+			conf: DefaultConfig,
 			args: args{
 				data: "123456",
 				hash: "$argon2i$v=19$m=32768,t=3,p=2$VDkrfTNOys4cBijO2rNTBw$2NP3RaDtHrXrMU+kKlcyTvxjyZOfHYoSAxmUjxS4w1Q",
@@ -142,8 +151,8 @@ func TestArgon2_Compare(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:   "argon2i invalid pwhasher",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2i invalid pwhasher",
+			conf: DefaultConfig,
 			args: args{
 				data: "qwerty",
 				hash: "$v=19$m=32768,t=3,p=2$VDkrfTNOys4cBijO2rNTBw$2NP3RaDtHrXrMU+kKlcyTvxjyZOfHYoSAxmUjxS4w1Q",
@@ -152,8 +161,8 @@ func TestArgon2_Compare(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name:   "argon2i incompatible version",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2i incompatible version",
+			conf: DefaultConfig,
 			args: args{
 				data: "qwerty",
 				hash: "$argon2i$v=10$m=32768,t=3,p=2$VDkrfTNOys4cBijO2rNTBw$2NP3RaDtHrXrMU+kKlcyTvxjyZOfHYoSAxmUjxS4w1Q",
@@ -162,8 +171,8 @@ func TestArgon2_Compare(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name:   "argon2id valid data",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2id valid data",
+			conf: DefaultConfig,
 			args: args{
 				data: "qwerty",
 				hash: "$argon2id$v=19$m=32768,t=3,p=2$7Jr8EtPeJsqJ1RxoxHC4eQ$XfSHQ28xgqc2/2LyE4YEkAI2CIilixOAvjh2Ds2s0+Y",
@@ -172,8 +181,8 @@ func TestArgon2_Compare(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:   "argon2id invalid data",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2id invalid data",
+			conf: DefaultConfig,
 			args: args{
 				data: "123456",
 				hash: "$argon2id$v=19$m=32768,t=3,p=2$7Jr8EtPeJsqJ1RxoxHC4eQ$XfSHQ28xgqc2/2LyE4YEkAI2CIilixOAvjh2Ds2s0+Y",
@@ -182,8 +191,8 @@ func TestArgon2_Compare(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:   "argon2id invalid pwhasher",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2id invalid pwhasher",
+			conf: DefaultConfig,
 			args: args{
 				data: "qwerty",
 				hash: "$m=32768,t=3,p=2$7Jr8EtPeJsqJ1RxoxHC4eQ$XfSHQ28xgqc2/2LyE4YEkAI2CIilixOAvjh2Ds2s0+Y",
@@ -192,8 +201,8 @@ func TestArgon2_Compare(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name:   "argon2id incompatible version",
-			fields: fields{conf: DefaultConfig},
+			name: "argon2id incompatible version",
+			conf: DefaultConfig,
 			args: args{
 				data: "qwerty",
 				hash: "$argon2id$v=17$m=32768,t=3,p=2$7Jr8EtPeJsqJ1RxoxHC4eQ$XfSHQ28xgqc2/2LyE4YEkAI2CIilixOAvjh2Ds2s0+Y",
@@ -204,8 +213,10 @@ func TestArgon2_Compare(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a := Argon2{
-				Ctx: tt.fields.conf,
+			a, err := initAdapter(nil, tt.conf)
+			if err != nil {
+				t.Errorf("initAdapter() error = %v", err)
+				return
 			}
 			got, err := a.ComparePw(tt.args.data, tt.args.hash)
 			if (err != nil) != tt.wantErr {
