f0a926ce8d992d0d81dcc71fac64a42f727a8a19
asaevich <semen.asaevich@gmail.com>
2021-03-31 22:09:10 +0300

    F28: Cookies for auth

--- internal/plugins/storage/types/feature_session.go
+++ internal/plugins/storage/types/feature_session.go
@@ -5,24 +5,20 @@ import (
 	"time"
 )
 
+type InsertSessionData struct {
+	UserId       int
+	SessionToken string
+	Expiration   time.Duration
+}
+
 type Session interface {
 	SetGCInterval(time.Duration)
 
 	CreateSessionColl(coll.Specification) error
 
-	// Get gets the value for the given key.
-	// `nil, nil` is returned when the key does not exist
-	Get(coll.Specification, string) ([]byte, error)
+	GetSession(coll.Specification, int) (JSONCollResult, error)
 
-	// Set stores the given value for the given key along
-	// with an expiration value, 0 means no expiration.
-	// Empty key or value will be ignored without an error.
-	Set(coll.Specification, string, []byte, time.Duration) error
+	InsertSession(coll.Specification, InsertSessionData) (JSONCollResult, error)
 
-	// Delete deletes the value for the given key.
-	// It returns no error if the storage does not contain the key,
-	Delete(coll.Specification, string) error
-
-	// Reset resets the storage and delete all keys.
-	Reset(coll.Specification) error
+	DeleteSession(coll.Specification, int) (JSONCollResult, error)
 }
--- plugins/storage/postgresql/feature_session.go
+++ plugins/storage/postgresql/feature_session.go
@@ -2,7 +2,10 @@ package postgresql
 
 import (
 	coll "aureole/internal/collections"
+	"aureole/internal/plugins/storage/types"
+	"context"
 	"fmt"
+	"github.com/jackc/pgx/v4"
 	"time"
 )
 
@@ -23,18 +26,66 @@ func (s *Storage) CreateSessionColl(spec coll.Specification) error {
 	return s.RawExec(sql)
 }
 
-func (s *Storage) Get(spec coll.Specification, key string) ([]byte, error) {
-	panic("implement me")
+func (s *Storage) GetSession(spec coll.Specification, userId int) (types.JSONCollResult, error) {
+	sql := fmt.Sprintf(`SELECT %s, %s FROM %s WHERE %s=$1;`,
+		Sanitize(spec.FieldsMap["session_id"]),
+		Sanitize(spec.FieldsMap["expiration"]),
+		Sanitize(spec.Name),
+		Sanitize(spec.Pk))
+
+	session, err := s.RawQuery(sql, userId)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	rawExp := session.(map[string]interface{})[spec.FieldsMap["expiration"]]
+	exp := rawExp.(int64)
+	if exp != 0 && exp <= time.Now().Unix() {
+		return nil, nil
+	}
+
+	return session, nil
 }
 
-func (s *Storage) Set(spec coll.Specification, key string, val []byte, exp time.Duration) error {
-	panic("implement me")
+func (s *Storage) InsertSession(spec coll.Specification, data types.InsertSessionData) (types.JSONCollResult, error) {
+	expSeconds := time.Now().Add(data.Expiration).Unix()
+	sql := fmt.Sprintf("INSERT INTO %s (%s, %s, %s) VALUES ($1, $2, $3) ON CONFLICT (%s) DO UPDATE SET v = $4, e = $5",
+		Sanitize(spec.Name),
+		Sanitize(spec.Pk),
+		Sanitize(spec.FieldsMap["session_id"]),
+		Sanitize(spec.FieldsMap["expiration"]),
+		Sanitize(spec.Pk))
+	return s.RawQuery(sql, data.UserId, data.SessionToken, expSeconds, data.SessionToken, expSeconds)
 }
 
-func (s *Storage) Delete(spec coll.Specification, key string) error {
-	panic("implement me")
+func (s *Storage) DeleteSession(spec coll.Specification, userId int) (types.JSONCollResult, error) {
+	sql := fmt.Sprintf("DELETE FROM %s WHERE %s=$1",
+		Sanitize(spec.Name),
+		Sanitize(spec.Pk))
+	return s.RawQuery(sql, userId)
 }
 
-func (s *Storage) Reset(spec coll.Specification) error {
-	panic("implement me")
+func (s *Storage) StartGC(spec coll.Specification) {
+	go s.gcTicker(spec)
+}
+
+// gcTicker starts the gc ticker
+func (s *Storage) gcTicker(spec coll.Specification) {
+	ticker := time.NewTicker(s.gcInterval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-s.gcDone:
+			return
+		case t := <-ticker.C:
+			sql := fmt.Sprintf("DELETE FROM %s WHERE %s <= $1 AND %s != 0",
+				Sanitize(spec.Name),
+				Sanitize(spec.FieldsMap["expiration"]),
+				Sanitize(spec.FieldsMap["expiration"]))
+			_, _ = s.conn.Exec(context.Background(), sql, t.Unix())
+		}
+	}
 }
