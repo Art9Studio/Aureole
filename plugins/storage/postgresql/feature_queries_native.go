3101466b76e95c3e97ca37d5d7843e6e2998fb2d
asaevich <semen.asaevich@gmail.com>
2021-10-01 13:22:18 +0300

    F103: Refactor jwt payload generating

--- plugins/storage/postgresql/feature_queries_native.go
+++ plugins/storage/postgresql/feature_queries_native.go
@@ -5,5 +5,7 @@ import (
 )
 
 func (s *Storage) NativeQuery(query string, args ...interface{}) (types.JSONCollResult, error) {
+	// todo: think about json aggregation func
+	// sql := fmt.Sprintf("select json_agg(t) from (%s) t", query)
 	return s.RawQuery(query, args...)
 }
